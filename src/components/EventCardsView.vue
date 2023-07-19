d5090997566944590bee10237a23c98d36471a13
Aleksey Miliekhin <miliekhin@printum.io>
2023-07-19 19:53:01 +0300

    add EventCardsView

---
+++ src/components/EventCardsView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { useEventStore } from "@/stores/eventStore";
+  import EventCard from "@/components/EventCard.vue";
+
+  const store = useEventStore();
+</script>
+
+<template>
+  <div class="flex flex-initial flex-wrap">
+    <EventCard
+      v-for="(ev, idx) in store.paginatedEvents"
+      :key="idx"
+      :event="ev"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
