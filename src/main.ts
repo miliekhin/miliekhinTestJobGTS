971099f04096d75a5fec7e89015448e0767fc543
Aleksey Miliekhin <miliekhin@printum.io>
2023-07-19 19:43:07 +0300

    upadate maints

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
+import router from './router';
 import PrimeVue from 'primevue/config';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
@@ -21,6 +22,7 @@ import OverlayPanel from 'primevue/overlaypanel';
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(router);
 app.use(PrimeVue);
 app.component('Avatar', Avatar);
 app.component('Paginator', Paginator);
