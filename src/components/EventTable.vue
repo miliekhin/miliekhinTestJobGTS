c761444dc9486056e35140a580b275fae3e7e632
Aleksey Miliekhin <miliekhin@printum.io>
2023-08-24 13:56:47 +0300

    fix import

--- src/components/EventTable.vue
+++ src/components/EventTable.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { DataTableRowSelectEvent } from "primevue/datatable";
+  import type { DataTableRowSelectEvent } from "primevue/datatable";
   import { useEventStore } from "@/stores/eventStore";
   import { inject, onMounted } from "vue";
 
